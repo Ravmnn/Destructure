576ee6a2bf2abd6601d633679595620c2af16975
Marvin Marjan <marvin.marjan@gmail.com>
2026-04-17 14:39:00 -0400

    initial setup

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include <raylib.h>
+
+
+
+
+int main()
+{
+    SetConfigFlags(FLAG_FULLSCREEN_MODE);
+    int monitor = GetCurrentMonitor();
+    InitWindow(GetMonitorWidth(monitor), GetMonitorHeight(monitor), "Destructure");
+
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(BLACK);
+
+            DrawCircleV(GetMousePosition(), 10.0f, WHITE);
+
+        EndDrawing();
+    }
+
+
+    CloseWindow();
+
+    return 0;
+}
